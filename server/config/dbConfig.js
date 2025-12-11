d65845eb592c4473d98895664f9f048c28ec6de7
Zik <zikrulla.zik@gmail.com>
2025-12-11 05:37:31 +0530

    db connection to our server

---
+++ server/config/dbConfig.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const dbString = 'mongodb+srv://zikrulla16:lZDLZfGOChpHmEkf@cluster0.hbqzke7.mongodb.net/BMS?appName=Cluster0';
+
+mongoose.connect(dbString);
+
+const connection = mongoose.connection;
+
+connection.on('connected', ()=>{
+    console.log("db connected successfully")
+})
+connection.on('error', ()=>{
+    console.log("failed to make the connection");
+})
+
+
+// alternative method
+// mongoose.connect(dbString).then(()=>{
+//     console.log('Connection Successful');
+// }).catch(()=>{
+//     console.log('connection unsuccessful')
+// })
+
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dbConfig = require('./config/dbConfig');
 
 const app = express();
 
