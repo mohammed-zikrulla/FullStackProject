3374b374e777aff3463bec96314d0cf7a433c3fe
Zik <zikrulla.zik@gmail.com>
2025-12-13 02:00:04 +0530

    Login routes added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,18 +3,18 @@ const User = require('../models/userModel');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-router.post('/register', async (req, res)=>{
-    
-    const userExists = await User.findOne({email: req.body.email})
-    if(userExists){
+router.post('/register', async (req, res) => {
+
+    const userExists = await User.findOne({ email: req.body.email })
+    if (userExists) {
         res.send({
             success: false,
             message: "user already exists"
         })
     }
-    
+
     // hasing and salting
-    const salt = bcrypt.genSaltSync(10);
+    const salt = await bcrypt.genSalt(10);
     console.log(salt);
 
     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
@@ -30,8 +30,22 @@ router.post('/register', async (req, res)=>{
     })
 })
 
-router.post('/login', async ()=>{
-
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    try {
+        if (user) {
+            res.send({
+                success: true,
+                message: 'User Logged in Successfully'
+            })
+        }
+        res.send({
+            success: false,
+            message: "User not found Plese Register"
+        })
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 module.exports = router;
