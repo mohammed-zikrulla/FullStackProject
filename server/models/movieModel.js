1df83005fc77631708c3c1b23b41971c78b4716e
Zik <zikrulla.zik@gmail.com>
2025-12-19 17:56:21 +0530

    movie Schema

---
+++ server/models/movieModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const movieSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: String,
+        required: true
+    },
+    releaseDate: {
+        type: Date,
+        required: true
+    },
+    // poster: {
+    //     type: String,
+    //     required: true
+    // }
+})
+
+const Movies = mongoose.model('movie', movieSchema);
+
+module.exports = Movies;
