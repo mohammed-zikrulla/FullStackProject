18339818279c21ee043a6c9a715440abed956a88
Zik <zikrulla.zik@gmail.com>
2026-01-01 23:42:47 +0530

    formatting

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const dbString = 'mongodb+srv://zikrulla16:lZDLZfGOChpHmEkf@cluster0.hbqzke7.mongodb.net/BMS?appName=Cluster0';
+const dbString =
+  "mongodb+srv://zikrulla16:lZDLZfGOChpHmEkf@cluster0.hbqzke7.mongodb.net/BMS?appName=Cluster0";
 
 mongoose.connect(dbString);
 
 const connection = mongoose.connection;
 
-connection.on('connected', ()=>{
-    console.log("db connected successfully")
-})
-connection.on('error', ()=>{
-    console.log("failed to make the connection");
-})
-
+connection.on("connected", () => {
+  console.log("db connected successfully");
+});
+connection.on("error", () => {
+  console.log("failed to make the connection");
+});
 
 // alternative method
 // mongoose.connect(dbString).then(()=>{
@@ -20,4 +20,3 @@ connection.on('error', ()=>{
 // }).catch(()=>{
 //     console.log('connection unsuccessful')
 // })
-
