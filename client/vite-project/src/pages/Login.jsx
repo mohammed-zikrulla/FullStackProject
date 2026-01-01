f26f6bf27b2961a9aaf1b0e7565da8b3a8cd7f01
Zik <zikrulla.zik@gmail.com>
2026-01-01 23:41:08 +0530

    formatting

--- client/vite-project/src/pages/Login.jsx
+++ client/vite-project/src/pages/Login.jsx
@@ -1,63 +1,73 @@
 import React from "react";
 import { Button, Form, Input } from "antd";
-import { Link } from 'react-router-dom'
-import "./auth.css";  // Custom CSS file for better styling
+import { Link } from "react-router-dom";
+import "./auth.css"; // Custom CSS file for better styling
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../apiCalls/users";
 
-
 function Login() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const onFinish = async (values) => {
-        try {
-            const response = await loginUser(values);
-            if(response.data.success){
-                const token = response.data.token;
-                localStorage.setItem("token", token);
-                    alert("Login Successful");
-                    navigate('/home');
-            }
-            else{
-                alert("Login Failed");
-            }
-        } catch (error) {
-            console.error(error);
-        }
+  const onFinish = async (values) => {
+    try {
+      const response = await loginUser(values);
+      if (response.data.success) {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        alert("Login Successful");
+        navigate("/home");
+      } else {
+        alert("Login Failed");
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    return (
-        <div className="auth-container">
-            <div className="auth-box">
-                <h1 className="title">Login to Zaka International</h1>
-                <Form layout="vertical" onFinish={onFinish}>
-                    <Form.Item
-                        label="Email"
-                        name="email"
-                        rules={[{ required: true, message: "Email is required" }]}
-                    >
-                        <Input type="email" placeholder="Enter your email" className="input-field" />
-                    </Form.Item>
+  return (
+    <div className="auth-container">
+      <div className="auth-box">
+        <h1 className="title">Login to Zaka International</h1>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Email is required" }]}
+          >
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              className="input-field"
+            />
+          </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: "Password is required" }]}
-                    >
-                        <Input type="password" placeholder="Enter your password" className="input-field" />
-                    </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Password is required" }]}
+          >
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              className="input-field"
+            />
+          </Form.Item>
 
-                    <Button type="primary" block htmlType="submit" className="auth-button">
-                        Login
-                    </Button>
-                </Form>
-                <p className="switch-text">
-                    New User? <Link to='/register'>Regsiter here</Link>
-                </p>
-            </div>
-        </div>
-    );
+          <Button
+            type="primary"
+            block
+            htmlType="submit"
+            className="auth-button"
+          >
+            Login
+          </Button>
+        </Form>
+        <p className="switch-text">
+          New User? <Link to="/register">Regsiter here</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
