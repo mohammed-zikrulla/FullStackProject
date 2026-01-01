41fd5792fcf83a1f2574289799dd63d3e349d1f7
Zik <zikrulla.zik@gmail.com>
2026-01-01 23:43:10 +0530

    formatting

--- client/vite-project/src/apiCalls/users.js
+++ client/vite-project/src/apiCalls/users.js
@@ -1,16 +1,16 @@
-import { axiosInstance } from "."
+import { axiosInstance } from ".";
 
-export const RegisterUser = async (values)=>{
-   const response = await axiosInstance.post('/api/users/register', values);
-   return response;
-}
+export const RegisterUser = async (values) => {
+  const response = await axiosInstance.post("/api/users/register", values);
+  return response;
+};
 
-export const loginUser = async (values)=>{
-   const response = await axiosInstance.post('/api/users/login', values);
-   return response;
-}
+export const loginUser = async (values) => {
+  const response = await axiosInstance.post("/api/users/login", values);
+  return response;
+};
 
-export const GetCurrentUser = async (values)=>{
-   const response = await axiosInstance.get('api/users/getValidUser', values);
-   return response;
-} 
+export const GetCurrentUser = async (values) => {
+  const response = await axiosInstance.get("api/users/getValidUser", values);
+  return response;
+};
