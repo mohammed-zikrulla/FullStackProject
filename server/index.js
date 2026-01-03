520f215df4cf6cc6e53334c2b081f52f9d9ab08e
Zik <zikrulla.zik@gmail.com>
2026-01-03 23:22:41 +0530

    fromatting

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const dbConfig = require('./config/dbConfig');
-const userRoutes = require('./routes/userRoutes')
-const movieRoutes = require('./routes/movieRoutes')
+const express = require("express");
+const dbConfig = require("./config/dbConfig");
+const userRoutes = require("./routes/userRoutes");
+const movieRoutes = require("./routes/movieRoutes");
 const app = express();
 
 //built in middleware
 app.use(express.json());
 
 //custom middleware
-app.use('/api/users', userRoutes);
-app.use('/api/movies', movieRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/movies", movieRoutes);
 
-app.listen(8082, ()=>{
-    console.log('server started on 8082');
-})
+app.listen(8082, () => {
+  console.log("server started on 8082");
+});
